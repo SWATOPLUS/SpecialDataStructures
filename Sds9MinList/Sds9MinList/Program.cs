a0d52ccae85f30300b32c4b155c263b90d18b942
Pavel Zhdanovich <swatoplus@gmail.com>
2019-10-19 16:49:00 +0300

    Sds9: Implementated StackMinList

--- Sds9MinList/Sds9MinList/Program.cs
+++ Sds9MinList/Sds9MinList/Program.cs
@@ -4,11 +4,11 @@ using System.Linq;
 
 namespace Sds9MinList
 {
-    class Program
+    internal class Program
     {
         private static void Main()
         {
-            var list = new MyList<int>();
+            var list = new StackMinList<int>();
 
             var commandCountLine = Console.ReadLine() ?? throw new InvalidOperationException();
             var commandCount = int.Parse(commandCountLine.Trim());
@@ -41,7 +41,117 @@ namespace Sds9MinList
             }
         }
 
-        private class MyList<T> where T : struct
+        private interface IMinList<T> where T : struct, IEquatable<T>
+        {
+            void PushFront(T x);
+            void PushBack(T x);
+            void PopFront();
+            void PopBack();
+            T? GetMin();
+        }
+
+        private class StackMinList<T> : IMinList<T> where T : struct, IEquatable<T>
+        {
+            private readonly Stack<(T Item, T Min)> _frontStack = new Stack<(T Item, T Min)>();
+            private readonly Stack<(T Item, T Min)> _backStack = new Stack<(T Item, T Min)>();
+
+            public void PushFront(T x)
+            {
+                var items = new List<T>();
+
+                if (_frontStack.Any())
+                {
+                    items.Add(_frontStack.Peek().Min);
+                }
+
+                items.Add(x);
+
+                _frontStack.Push((x, items.Min()));
+            }
+
+            public void PushBack(T x)
+            {
+                var items = new List<T>();
+
+                if (_backStack.Any())
+                {
+                    items.Add(_backStack.Peek().Min);
+                }
+
+                items.Add(x);
+
+                _backStack.Push((x, items.Min()));
+            }
+
+            public void PopFront()
+            {
+                if (_frontStack.Any())
+                {
+                    _frontStack.Pop();
+                }
+                else
+                {
+                    CutAndRebase(true);
+                }
+            }
+
+            public void PopBack()
+            {
+                if (_backStack.Any())
+                {
+                    _backStack.Pop();
+                }
+                else
+                {
+                    CutAndRebase(false);
+                }
+            }
+
+            private void CutAndRebase(bool isFront)
+            {
+                var items = _frontStack.Concat(_backStack.Reverse()).ToArray();
+
+                var cutItems = isFront
+                    ? items.Skip(1).Select(x => x.Item).ToArray()
+                    : items.Take(items.Length - 1).Select(x => x.Item).ToArray();
+
+                var frontItemsCount = cutItems.Length / 2;
+
+                _frontStack.Clear();
+                _backStack.Clear();
+
+                foreach (var item in cutItems.Take(frontItemsCount))
+                {
+                    PushFront(item);
+                }
+
+                foreach (var item in cutItems.Skip(frontItemsCount).Reverse())
+                {
+                    PushBack(item);
+                }
+            }
+
+            public T? GetMin()
+            {
+                var items = new List<T>();
+
+                if (_frontStack.Any())
+                {
+                    items.Add(_frontStack.Peek().Min);
+                }
+
+                if (_backStack.Any())
+                {
+                    items.Add(_backStack.Peek().Min);
+                }
+
+                return items.Count > 0
+                    ? items.Min()
+                    : null as T?;
+            }
+        }
+
+        private class LinkedMinList<T> : IMinList<T> where T : struct, IEquatable<T>
         {
             private readonly LinkedList<T> _list = new LinkedList<T>();
 
