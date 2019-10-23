54442952fbe2045c0018a30e1c20b68ca4cdf921
Pavel Zhdanovich <swatoplus@gmail.com>
2019-10-23 22:00:36 +0300

    Sds45: Added Accepted Solution

---
+++ Sds45MegaInversions/Sds45MegaInversions.TestGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+
+namespace Sds45MegaInversions.TestGenerator
+{
+    internal static class Program
+    {
+        private const string OutputFileName = "input.txt";
+        private const int TestSize = 100_000;
+        private static readonly Random Random = new Random(42);
+
+        private static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, TestSize)
+                .Select(x => x + 1)
+                .Shuffle(Random)
+                .Select(x => x.ToString());
+
+            var lines = new[]
+            {
+                TestSize.ToString(),
+            }.Concat(items);
+
+            File.WriteAllLines(OutputFileName, lines);
+        }
+    }
+}
---
+++ Sds45MegaInversions/Sds45MegaInversions/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Sds45MegaInversions
+{
+    internal static class Program
+    {
+        private const string InputFileName = "input.txt";
+        private const string OutputFileName = "output.txt";
+
+        private static void Main()
+        {
+            var input = File.ReadAllLines(InputFileName);
+
+            var size = int.Parse(input[0]);
+
+            var orderTree = new FenwickTree(size);
+            var inversionsTree = new FenwickTree(size);
+
+            var result = 0L;
+
+            foreach (var order in input.Skip(1).Select(int.Parse))
+            {
+                orderTree.Add(order, 1);
+
+                var inversions = orderTree.Query(size) - orderTree.Query(order);
+
+                inversionsTree.Add(order, inversions);
+
+                result += inversionsTree.Query(size) - inversionsTree.Query(order);
+            }
+
+            File.WriteAllText(OutputFileName, result.ToString());
+        }
+
+        private class FenwickTree
+        {
+            private readonly long[] _tree;
+
+            public FenwickTree(int n)
+            {
+                _tree = new long[RoundUpPow2(2 * n) + 1];
+            }
+
+            public void Add(int k, long x)
+            {
+                if (k == 0)
+                {
+                    return;
+                }
+
+                while (k < _tree.Length)
+                {
+                    _tree[k] += x;
+
+                    k += LowBit(k);
+                }
+            }
+
+            public long Query(int k)
+            {
+                var sum = 0L;
+
+                while (k > 0)
+                {
+                    sum += _tree[k];
+
+                    k -= LowBit(k);
+                }
+
+                return sum;
+            }
+
+            private static int RoundUpPow2(int n)
+            {
+                var up = 1;
+
+                while (up < n)
+                {
+                    up *= 2;
+                }
+
+                return up;
+            }
+
+            private static int LowBit(int n)
+            {
+                return n & -n;
+            }
+        }
+    }
+}
