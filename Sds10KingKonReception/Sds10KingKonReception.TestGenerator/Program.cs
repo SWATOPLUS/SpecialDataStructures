395de4e6a5e2b0966174dcf03ecbede72e903486
Pavel Zhdanovich <swatoplus@gmail.com>
2019-10-27 20:40:32 +0300

    Sds10: Added generator and trivial solution

---
+++ Sds10KingKonReception/Sds10KingKonReception.TestGenerator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Sds10KingKonReception.TestGenerator
+{
+    internal class Program
+    {
+        private const string OutputFileName = "input.txt";
+        private static int QueryCount = 100_000;
+        private static int MaxInTime = 1_000_000;
+        private static int MaxDurationTime = 1_000_000;
+
+        private static readonly Random Random = new Random();
+
+        private static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                QueryCount = int.Parse(args[0]);
+                MaxInTime = int.Parse(args[1]);
+                MaxDurationTime = int.Parse(args[2]);
+            }
+
+            var commands = BuildCommands();
+            var lines = new[] {QueryCount.ToString()}
+                .Concat(commands);
+
+            File.WriteAllLines(OutputFileName, lines);
+        }
+
+        private static IEnumerable<string> BuildCommands()
+        {
+            var inTimes = new Dictionary<int, int>();
+
+            foreach (var index in Enumerable.Range(0, QueryCount))
+            {
+                var randomRange = inTimes.Any() ? 3 : 2;
+
+                var result = null as string;
+
+                switch (Random.Next(randomRange))
+                {
+                    case 0:
+                        result = $"? {Random.Next(1, MaxInTime + 1)}";
+                        break;
+                    case 1:
+                        var possibleInTimes = Enumerable
+                            .Range(1, MaxInTime)
+                            .Except(inTimes.Keys)
+                            .ToArray();
+
+                        var inTimeIndex = Random.Next(possibleInTimes.Length);
+                        var inTime = possibleInTimes[inTimeIndex];
+                        inTimes.Add(inTime, index);
+
+                        result = $"+ {inTime} {Random.Next(1, MaxDurationTime + 1)}";
+                        break;
+                    case 2:
+                        var skipTime = inTimes.ToArray()[Random.Next(inTimes.Count)];
+                        inTimes.Remove(skipTime.Key);
+
+                        result = $"- {skipTime.Value + 1}";
+                        break;
+                        
+                }
+
+                yield return result;
+            }
+        }
+
+    }
+}
---
+++ Sds10KingKonReception/Sds10KingKonReception/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sds10KingKonReception
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var inputArgs = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            IKingKonReception reception = new TrivialKingKonReception();
+
+            var outputBuilder = new StringBuilder();
+
+            var queryCount = inputArgs[0];
+
+            var joinTime = new int[queryCount];
+
+            foreach (var index in Enumerable.Range(0, queryCount))
+            {
+                var commandParts = Console.ReadLine()
+                    .Split(' ');
+
+                var command = commandParts.First();
+
+                var commandArgs = commandParts.Skip(1)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                switch (command)
+                {
+                    case "+":
+                        joinTime[index] = commandArgs[0];
+                        reception.Join(commandArgs[0], commandArgs[1]);
+                        break;
+                    case "-":
+                        reception.Cancel(joinTime[commandArgs[0] - 1]);
+                        break;
+                    case "?":
+                        var result = reception.Query(commandArgs[0]);
+                        outputBuilder.AppendLine(result.ToString());
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+
+            Console.Write(outputBuilder.ToString());
+        }
+
+        private interface IKingKonReception
+        {
+            void Join(int time, int duration);
+
+            void Cancel(int time);
+
+            long Query(int time);
+        }
+
+        private class TrivialKingKonReception : IKingKonReception
+        {
+            private readonly Dictionary<int, int> _timeDurationDictionary = new Dictionary<int, int>();
+ 
+            public void Join(int time, int duration)
+            {
+                _timeDurationDictionary.Add(time, duration);
+            }
+
+            public void Cancel(int time)
+            {
+                _timeDurationDictionary.Remove(time);
+            }
+
+            public long Query(int time)
+            {
+                var result = QueryAbsolute(time);
+
+                return Math.Max(0, result - time);
+            }
+
+            private long QueryAbsolute(int queryTime)
+            {
+                var timeStamp = 0L;
+
+                foreach (var join in _timeDurationDictionary.OrderBy(x => x.Key))
+                {
+                    var time = join.Key;
+                    var duration = join.Value;
+
+                    if (queryTime < time)
+                    {
+                        return timeStamp;
+                    }
+
+                    if (timeStamp < time)
+                    {
+                        timeStamp = time;
+                    }
+
+                    timeStamp += duration;
+                }
+
+                return timeStamp;
+            }
+        }
+
+        private class KingKonReception : IKingKonReception
+        {
+            public void Join(int time, int duration)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Cancel(int time)
+            {
+                throw new NotImplementedException();
+            }
+
+            public long Query(int time)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
